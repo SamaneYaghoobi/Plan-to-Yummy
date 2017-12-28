080e867d133a4594fee6330c7c88fceccd4ee331
Samane <samaneyaghoobi1996@gmail.com>
2017-12-29 01:06:58 +0330

    Minify recipe model

--- src/app/recipes/recipes.model.ts
+++ src/app/recipes/recipes.model.ts
@@ -1,11 +1,3 @@
 export class Recipe {
-    public name: string;
-    public description: string;
-    public imagePath: string;
-
-    constructor(name: string, desc: string, imagePath: string) {
-        this.name = name;
-        this.description = desc;
-        this.imagePath = imagePath;
-    }
+    constructor(public name: string, public desc: string, public imagePath: string) {}
 }
