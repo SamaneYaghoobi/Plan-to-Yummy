7d8aa3f9ff1fc7d79efefe0b26f3bf9a1b6e7188
Samane <samaneyaghoobi1996@gmail.com>
2018-01-06 00:13:28 +0330

    Add recipe Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,22 @@
 import { Recipe } from './../recipes.model';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
+  providers: [RecipeService]
 })
 export class RecipeListComponent implements OnInit {
 
   @Output() selectedRecipes = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('test', 'this is test', 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/6/3/1/FNM_070111-Fried-Chicken-026_s4x3.jpg.rend.hgtvcom.966.725.suffix/1382539796174.jpeg'),
-    new Recipe('testing', 'this is test', 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/6/3/1/FNM_070111-Fried-Chicken-026_s4x3.jpg.rend.hgtvcom.966.725.suffix/1382539796174.jpeg')
-  ];
-  constructor() { }
+  recipes: Recipe[];
+  
+  constructor(private recipeService: RecipeService) {
+    this.recipes = recipeService.getRecipe();
+  }
 
   selectedRecipe(recipe: Recipe) {
     this.selectedRecipes.emit(recipe);
