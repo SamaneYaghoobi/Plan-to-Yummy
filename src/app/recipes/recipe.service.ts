626fb50c8bed84375cb1fe6ed4fad4e06879a6a1
Samane <samaneyaghoobi1996@gmail.com>
2018-01-06 21:14:32 +0330

    Add ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,24 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipes.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
     selectedRecipe = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('test', 'this is test', 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/6/3/1/FNM_070111-Fried-Chicken-026_s4x3.jpg.rend.hgtvcom.966.725.suffix/1382539796174.jpeg'),
-        new Recipe('testing', 'this is test', 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/6/3/1/FNM_070111-Fried-Chicken-026_s4x3.jpg.rend.hgtvcom.966.725.suffix/1382539796174.jpeg')
+        new Recipe('test',
+            'this is test',
+            'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/6/3/1/FNM_070111-Fried-Chicken-026_s4x3.jpg.rend.hgtvcom.966.725.suffix/1382539796174.jpeg'
+            , [(new Ingredient('meat', 1)),
+            new Ingredient('breaad', 2)]
+        ),
+        new Recipe('testing',
+            'this is test',
+            'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/6/3/1/FNM_070111-Fried-Chicken-026_s4x3.jpg.rend.hgtvcom.966.725.suffix/1382539796174.jpeg'
+            , [(new Ingredient('meat', 1)),
+            new Ingredient('breads', 2)]
+        )
     ];
 
     getRecipe() {
--- src/app/recipes/recipes.model.ts
+++ src/app/recipes/recipes.model.ts
@@ -1,3 +1,8 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
-    constructor(public name: string, public desc: string, public imagePath: string) {}
+    constructor(public name: string,
+        public desc: string,
+        public imagePath: string,
+        public ingredients: Ingredient[]) { }
 }
